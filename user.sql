f7aec4805084d9acb98dce3748b091d30004c633
jaffsha <shahruzaj@gmail.com>
2024-04-09 12:06:35 -0500

    Add files via upload

---
+++ user.sql
@@ -0,0 +1,15 @@
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `FullName` varchar(100) DEFAULT NULL,
+  `AdminEmail` varchar(120) DEFAULT NULL,
+  `UserName` varchar(100) NOT NULL,
+  `Password` varchar(100) NOT NULL,
+  `updationDate` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id`, `FullName`, `AdminEmail`, `UserName`, `Password`, `updationDate`) VALUES
+(1, 'Anuj Kumar', 'admin@gmail.com', 'user', 'f925916e2754e5e03f75dd58a5733251', '2024-01-20 06:03:56');
