647b8f0d939171d99a97b0a51fa734ee7e05e074
LL302 <105566647+LL302@users.noreply.github.com>
2024-04-16 01:06:10 -0500

    Login Changes
    Changed the filenames and such so the login system works

--- login.php
+++ login.php
@@ -1,25 +1,2 @@
-<<<<<<< Updated upstream
-=======
-<!DOCTYPE html>
-<html>
-  <head>
-		<title>Login</title>
-    <link href="style.css" type="text/css" rel="stylesheet"</link>
-  </head>
-	<body>
-    <h1 class="titleLogin">Virtual Library</h1>
-    <h2 class="welcome">Welcome to Virtual Library!</h2>
 
-    <div class="login">
-    <h2>Login</h2>
-      <form action="tbd.php" method="post">
-          <input type="text" name="username" placeholder="Username" required>
-          <input type="password" name="password" placeholder="Password" required>
-          <input type="submit" value="Login">
-          <a href="create.php">Or create account</a>
-      </form>
-    </div>
-  </body>
-</html>
 		
->>>>>>> Stashed changes
