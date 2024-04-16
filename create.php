9eae2ba09b66570ad38bb6a865d4ae90415af5c0
LL302 <105566647+LL302@users.noreply.github.com>
2024-04-15 23:38:22 -0500

    Login created, dashboard created, library page crated, styles added

---
+++ create.php
@@ -0,0 +1,16 @@
+<<<<<<< Updated upstream
+=======
+<!DOCTYPE html>
+<html>
+  <head>
+    <!-- 1. This is my title. -->
+		<title>Create</title>
+    <link href="style.css" type="text/css" rel="stylesheet"</link>
+  </head>
+	<body>
+    <h1 class="titleLogin">Virtual Library</h1>
+    <h2 class="welcome">Welcome to Virtual Library!</h2>
+
+</html>
+		
+>>>>>>> Stashed changes
