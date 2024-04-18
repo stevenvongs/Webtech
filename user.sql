4d6b64e1855d3f149953861a10b271fb9c327439
jaffsha <shahruzaj@gmail.com>
2024-04-18 12:08:43 -0500

    user.sql

--- user.sql
+++ user.sql
@@ -1,26 +1,18 @@
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
 CREATE DATABASE UserName;
 
 CREATE TABLE `user` (
   `id` int(11) NOT NULL,
-  `FullName` varchar(100) DEFAULT NULL,
-  `UserEmail` varchar(120) DEFAULT NULL,
   `UserName` varchar(100) NOT NULL,
   `Password` varchar(100) NOT NULL,
-  `updationDate` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
 -- Dumping data for table `user`
 --
 
-INSERT INTO `user` (`id`, `FullName`, `AdminEmail`, `UserName`, `Password`, `updationDate`) VALUES
-(1, 'Anuj Kumar', 'admin@gmail.com', 'user', 'f925916e2754e5e03f75dd58a5733251', '2024-01-20 06:03:56');
-INSERT INTO `user` (`id`, `FullName`, `AdminEmail`, `UserName`, `Password`, `updationDate`) VALUES
-(2, 'John Doe', 'admin@gmail.com', 'johndoe', '8d7f51fb7b3c68a4051bd915b573d9e2', '2024-04-17 08:30:00');
-INSERT INTO `user` (`id`, `FullName`, `AdminEmail`, `UserName`, `Password`, `updationDate`) VALUES
-(3, 'Emily Smith', 'admin@gmail.com', 'emilysmith', 'a2b4f6c8e0', '2024-04-17 10:45:00');
+INSERT INTO `user` (`id`, `UserName`, `Password`) VALUES
+(1, 'johnsmith', 'john123');
+INSERT INTO `user` (`id`, `UserName`, `Password`) VALUES
+(2, 'johndoe', 'john1234');
+INSERT INTO `user` (`id`, `UserName`, `Password`) VALUES
+(3, 'emilysmith', 'ilovedog1');
 
