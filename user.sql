02ca0968b09a9fca862bef3fc6aa52df8e60f3e9
jaffsha <shahruzaj@gmail.com>
2024-04-17 21:46:35 -0500

    user.sql

--- user.sql
+++ user.sql
@@ -19,3 +19,8 @@ CREATE TABLE `user` (
 
 INSERT INTO `user` (`id`, `FullName`, `AdminEmail`, `UserName`, `Password`, `updationDate`) VALUES
 (1, 'Anuj Kumar', 'admin@gmail.com', 'user', 'f925916e2754e5e03f75dd58a5733251', '2024-01-20 06:03:56');
+INSERT INTO `user` (`id`, `FullName`, `AdminEmail`, `UserName`, `Password`, `updationDate`) VALUES
+(2, 'John Doe', 'admin@gmail.com', 'johndoe', '8d7f51fb7b3c68a4051bd915b573d9e2', '2024-04-17 08:30:00');
+INSERT INTO `user` (`id`, `FullName`, `AdminEmail`, `UserName`, `Password`, `updationDate`) VALUES
+(3, 'Emily Smith', 'admin@gmail.com', 'emilysmith', 'a2b4f6c8e0', '2024-04-17 10:45:00');
+
