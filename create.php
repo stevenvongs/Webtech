94b3ebef55a6312b8f3d829a652de486d83ae3b0
tby2b <tby2b@mtmail.mtsu.edu>
2024-04-18 12:15:52 -0500

    update

--- create.php
+++ create.php
@@ -1,6 +1,21 @@
     <?php
         // define variables and set to empty values
         $userName = $passWord = $vPass = "";
+
+        $host = "localhost";
+        $dbname = "login_db";
+        $username = "root";
+        $password = "";
+
+        $mysqli = new mysqli(
+        hostname: $host,
+        username: $username,
+        password: $password, 
+        database: $dbname);
+
+        if ($mysqli->connect_errno) {
+            die("Connection Error: " . $mysqli->connect_errno);
+        }
         
         if ($_SERVER["REQUEST_METHOD"] == "POST") {
             if (empty($_POST["username"])) {
@@ -18,9 +33,15 @@
             if($_POST["password"] !== $_POST["Vpassword"]) {
                 die("Passwords Do Not Match");
             }
+        } {
+
+        } else {
+
         }
             $passWord = password_hash($_POST["password"], PASSWORD_DEFAULT);
             print_r($_POST);
             var_dump($passWord)
+
+
 
         ?>
---
+++ user.sql
@@ -0,0 +1,17 @@
+CREATE DATABASE UserName;
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `UserName` varchar(100) NOT NULL,
+  `Password` varchar(100) NOT NULL,
+);
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id`, `UserName`, `Password`) VALUES
+(1, 'johnsmith', 'john123');
+INSERT INTO `user` (`id`, `UserName`, `Password`) VALUES
+(2, 'johndoe', 'john1234');
+INSERT INTO `user` (`id`, `UserName`, `Password`) VALUES
+(3, 'emilysmith', 'ilovedog1');
