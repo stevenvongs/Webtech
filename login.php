231364a6e62290e7004f679a7a700a4e292de42e
LL302 <105566647+LL302@users.noreply.github.com>
2024-04-19 18:12:19 -0500

    admin login, login/create changes
    -Admin login created
    -Dropdowns for switching between user and admin login created
    -deleted login.html

--- login.php
+++ login.php
@@ -3,9 +3,20 @@
   <head>
 		<title>Login</title>
     <link href="style.css" type="text/css" rel="stylesheet"</link>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
   </head>
 	<body>
-
+    <div class="dropdown">
+      <button type="button" class="btn btn-primary dropdown-toggle" id ="drop" data-bs-toggle="dropdown">
+        Account
+      </button>
+      <ul class="dropdown-menu">
+        <li><a class="dropdown-item" href="login.php">User</a></li>
+        <li><a class="dropdown-item" href="admin.php">Admin</a></li>
+      </ul>
+    </div>
+    
     <h1 class="titleLogin">Virtual Library</h1>
     <h2 class="welcome">Welcome to Virtual Library!</h2>
 
