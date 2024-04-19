d1c7e046c4ccf4f0e5c914c660a5bb12d85e5046
LL302 <105566647+LL302@users.noreply.github.com>
2024-04-19 18:24:34 -0500

    Admin pages created

---
+++ adminLibrary.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+  <head>
+		<title>Admin's Library</title>
+    <link href="style.css" type="text/css" rel="stylesheet"</link>
+  </head>
+	<body>
+    <h1 class="title">Virtual Library</h1>
+    <h2 class="welcome" style="padding: 0px;">Administrator</h2>
+
+    <!-- This is my navigation bar. -->   
+    <ul class="navBar">
+  	    <li class="navBarList"><a class="navBarElement" href="adminLibrary.php" target="_self">Library</a></li>
+        <li class="navBarList"><a class="navBarElement" href="adminRequests.php" target="_self">Requests</a></li>
+        <li class="navBarList"><a class="navBarElement" href="adminUsers.php" target="_self">Users</a></li>
+        <li class="navBarListLog"><a class="navBarElement" href="library.php" target="_self">Log Out</a></li>
+	</ul>
+	</body>
+</html>
