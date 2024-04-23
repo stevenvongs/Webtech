9fdf1d21c30a2327aa7aabc15efbd3f2346fa517
tby2b <158497776+tby2b@users.noreply.github.com>
2024-04-23 11:57:24 -0500

    create account and request updates

--- request.php
+++ request.php
@@ -6,7 +6,7 @@
 <html>
   <head>
 		<title>Dashboard</title>
-    <link href="style.css" type="text/css" rel="stylesheet"</link>
+    <link href="style.css" type="text/css" rel="stylesheet">
   </head>
 	<body>
     <h1 class="title">Virtual Library</h1>
@@ -16,6 +16,16 @@
   	    <li class="navBarList"><a class="navBarElement" href="library.php" target="_self">Library</a></li>
         <li class="navBarList"><a class="navBarElement" href="request.php" target="_self">Request</a></li>
         <li class="navBarListLog"><a class="navBarElement" href="library.php" target="_self">Log Out</a></li>
-	</ul>
+	  </ul>
+
+    <div class="login">
+      <h2>Create A Book Request</h2>
+      <form action="requestSubmit.php" method="post">
+          <input type="text" name="bookAuthor" placeholder="Book Author" required>
+          <input type="text" name="bookTitle" placeholder="Book Title" required>
+          <input type="submit" value="Request a Book!">
+          <a href="library.php">Check out Our Library</a>
+      </form>
+    </div>
 	</body>
 </html>
